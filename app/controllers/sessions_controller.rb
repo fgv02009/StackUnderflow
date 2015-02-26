e5dd4aafffb63e6f4d0396de260660c7403efc9c
Kevin Yoon & Sarah Ing <pair+kyoon105.sarahikeda@devbootcamp.com>
2015-02-26 14:58:26 -0600

    add some sessions stuff oops

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,1 +1,9 @@
-sessions_controller.rb
+class SessionsController < ApplicationController
+  def new 
+    @user = User.new
+  end
+
+  def create
+    @user = User.find_by_email(params[:email].try(:authenticate, params[:password]))
+    if @user
+      session[:user_id] = @user.id
