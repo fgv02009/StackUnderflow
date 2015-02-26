5ab7fc69e0f47a4e03a8ce26ac6b07dc72e55910
Kevin Yoon & Sarah Ing <pair+kyoon105.sarahikeda@devbootcamp.com>
2015-02-26 15:10:11 -0600

    Made controllers for sessions and users. And routes for sessions for users

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,13 @@
 Rails.application.routes.draw do
+  get "/login"   => "sessions#new"
+  post "/login"  => "sessions#create"
+  get "/logout"  => "sessions#destroy"
+
+  resources :users
+
+  root 'questions#index'
+
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
