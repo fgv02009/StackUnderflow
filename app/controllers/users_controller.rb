f967a1ea24d47ac8ef1c961e11c8c26426cd1ca7
Kevin Yoon & Sarah Ing <pair+kyoon105.sarahikeda@devbootcamp.com>
2015-02-26 15:41:36 -0600

    modified controller so login function works, w user hash

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,8 +6,10 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      session[:user_id] = @user.id
       redirect_to root_path
     else
+      @user.errors
       render "new"
     end
   end
