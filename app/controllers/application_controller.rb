f7f210db5a2e75fd3e69b6d0cf206a0140eb0c64
fgv02009 <flori.garciavicente@gmail.com>
2015-02-27 14:58:23 -0600

    remove not used helper method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,7 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  helper_method :current_user, :markdown, :syntax_highlighter
+  helper_method :current_user, :markdown
   def current_user
     @_user || User.find(session[:user_id]) if session[:user_id]
   end
