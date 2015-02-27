83b07ae767cd0f6da7692be880258a08209ae57b
Kevin Yoon & Sarah Ing <pair+kyoon105.sarahikeda@devbootcamp.com>
2015-02-26 18:04:43 -0600

    add new files from master

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -5,22 +5,45 @@ describe User do
 
   describe "validations" do
     it "is invalid when there is no username" do
-
+      user.email = 'sarahikeda@gmail.com'
+      user.valid?
+      expect(user.errors[:username]).to_not be_empty
     end
 
     it "is invalid when there is no email" do
+      user.username = 'kyoon105hope'
+      user.valid?
+      expect(user.errors[:email]).to_not be_empty
     end
 
     it "is invalid when username is duplicative" do
+      new_user = User.new(username: "kyoon105hope", email: "mail@mail.com", password: "password")
+      new_user.save
+      user.username = "kyoon105hope"
+      user.valid?
+      expect(user.errors[:username]).to_not be_empty
     end
 
     it "is invalid when email is duplicative" do
+      new_user = User.new(username: "kyoon105hope", email: "mail@mail.com", password: "password")
+      new_user.save
+      user.email = "mail@mail.com"
+      user.valid?
+      expect(user.errors[:email]).to_not be_empty
     end
 
     it "is invalid when password is absent" do
+      user.username = "kyoon105hope"
+      user.email = "mail@mail.com"
+      user.valid?
+      expect(user.errors[:password]).to_not be_empty
     end
 
     it "is invalid when passwords do not match" do
+      user.password = "password"
+      user.password_confirmation = "WRONG"
+      user.valid?
+      expect(user.errors[:password_confirmation]).to_not be_empty
     end
   end 
 
