7444b71be3152b2c10d93acb07832a76e5e16efd
fgv02009 <flori.garciavicente@gmail.com>
2015-02-26 19:23:35 -0600

    user testing

--- spec/features/stackunderflow_spec.rb
+++ spec/features/stackunderflow_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-feature 'visitor visits website' do
+feature 'Visitor visits website' do
   scenario "sees a list of 10 questions" do
     visit '/'
     expect(page).to have_content("10 Most Recent Questions")
@@ -12,5 +12,34 @@ feature 'visitor visits website' do
     visit '/'
     click_link("most recent q")
     expect(page).to have_content("winning")
+  end
+
+  xscenario "visitor can't add a question" do
+  end
+end
+
+feature 'User visits website ' do
+  before(:all) do
+    visit '/'
+    click_link("login")
+    user = User.create!(username: Faker::Name.name, email: Faker::Internet.email, password: "password")
+    #FILL IN FORM
+  end
+  xscenario "can login" do
+    expect(page).to have_content(user.username)
+  end
+  
+  scenario "sees a list of 10 questions" do
+    expect(page).to have_content("10 Most Recent Questions")
+  end
+
+  scenario "can create a new question" do
+    click_button("Ask a Question")
+    within ("#new_question") do
+      fill_in 'title', :with => 'New Fake Title'
+      fill_in 'content', :with => 'New Fake Content'
+    end
+    click_button ("Submit New Question")
+    expect("#table").to have_content('New Fake Content')
   end
 end
