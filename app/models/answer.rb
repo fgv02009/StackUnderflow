39950ee0ee6c8ae665a0c30cc9cc80816ac732e4
Kevin Yoon & Sarah Ing <pair+kyoon105.sarahikeda@devbootcamp.com>
2015-02-27 11:37:37 -0600

    add vote migration, model and seed. edited all other models.

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -1,6 +1,8 @@
 class Answer < ActiveRecord::Base
   belongs_to :user
   belongs_to :question
+  has_many :votes, as: :voteable
+  has_many :voters, through: :votes, source: :user
 
   validates_presence_of :content, :question_id, :user_id
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,10 @@ class User < ActiveRecord::Base
   has_secure_password
   has_many :answers
   has_many :questions
+  has_many :votes
+  has_many :voted_answers, through: :votes, source: :voteable, source_type: "Answer"
+  has_many :voted_questions, through: :votes, source: :voteable, source_type: "Question"
+
 
   validates_presence_of :username, :email
   validates_uniqueness_of :username, :email
