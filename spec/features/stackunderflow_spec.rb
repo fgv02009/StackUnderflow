2c846a5acf406d76a38eff1fc96dea491da361bc
fgv02009 <flori.garciavicente@gmail.com>
2015-02-26 18:55:27 -0600

    test for visitor to visit question show page

--- spec/features/stackunderflow_spec.rb
+++ spec/features/stackunderflow_spec.rb
@@ -5,4 +5,12 @@ feature 'visitor visits website' do
     visit '/'
     expect(page).to have_content("10 Most Recent Questions")
   end
+
+  scenario "can see a specific questions show page" do
+    user = User.create!(username: Faker::Name.name, email: Faker::Internet.email, password: "password")
+    question = Question.create!(title: "most recent q", content: "winning" ,user: user)
+    visit '/'
+    click_link("most recent q")
+    expect(page).to have_content("winning")
+  end
 end
