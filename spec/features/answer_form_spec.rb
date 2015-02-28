6bb7d2cae7c32d93ae91896ef8ac9057a41fbae0
fgv02009 & Sarah Ing <pair+fgv02009.sarahikeda@devbootcamp.com>
2015-02-28 16:11:33 -0600

    add all tests for answer form features

--- spec/features/answer_form_spec.rb
+++ spec/features/answer_form_spec.rb
@@ -2,18 +2,47 @@ require 'rails_helper'
 
 feature 'Can click question and view answer' do
   given(:user)  {User.create!(username: "Bob Rajput", email: Faker::Internet.email, password: "123")}
-  given(:question) {Question.create!(user: user, title: 'new fake title', content: 'new fake content')}
-  given(:answer) {Answer.create!(content: 'fake answer content', user: user, question: question)}
   scenario "show page populates", js: true do
-    visit '/'
+    generate_q
+    click_link("New Fake Title")
+      expect(page).to have_content('New Fake Content')
+  end
+
+  scenario "can answer question", js: true do
+    generate_q
+    click_link("New Fake Title")
+    click_link("Answer Question")
+    expect(find('#newanswer')).to have_css("#answer_content")
+  end
+
+  scenario "can submit answer", js: true do
+   generate_q
+    click_link("New Fake Title")
+    click_link("Answer Question")
+    expect(find('#newanswer')).to have_css("#answer_content")
+    within (".new_answer") do
+      fill_in 'Type your answer here.', :with => 'New Fake Answer'
+    end
+    click_button("Submit New Answer")
+    expect(page).to_not have_css("#answer_content")
+    expect(page).to have_content("New Fake Answer")
+  end
+end
+
+
+def generate_q
+ visit '/'
     click_link("Login")
     within(".new_user") do
       fill_in 'Email', :with => user.email
       fill_in 'Password', :with => user.password
       click_button("Login")
     end
-  require 'pry'; binding.pry
-    click_link("new fake title")
-      expect(page).to have_content('fake answer content')
-  end
+    click_link("Ask a Question")
+      within (".new_question") do
+        fill_in 'title', :with => 'New Fake Title'
+        fill_in 'content', :with => 'New Fake Content'
+      end
+    click_button ("Submit New Question")
 end
+ 
